27a2488d6c6c7c25f86942a16d9c2fe64c798423
miamactavish <miamactavish@gmail.com>
2016-03-31 14:31:28 -0600

    Bill screen mostly implemented

--- horizontal/horizontal/Bill.xaml.cs
+++ horizontal/horizontal/Bill.xaml.cs
@@ -33,23 +33,25 @@ namespace horizontal
             profiles = new List<String>();
             profiles.Add("Mia");
             profiles.Add("Ryan");
+            profiles.Add("Leo");
 
             orders = new List<BillItem>();
-            orders.Add(new BillItem("Roast Chicken", new List<String>() { "Mia" }, "$25.00"));
-            orders.Add(new BillItem("Steak", new List<String>() { "Ryan" }, "$30.00"));
-            orders.Add(new BillItem("Dessert", new List<String>() { "Mia", "Ryan" }, "$10.00", "No whipped cream", "$0.00"));
+            orders.Add(new BillItem("Roast Chicken", new List<String>() { "Mia", "Leo" }, 25.00F, "Subsititute sweet potato fries", 3.00F));
+            orders.Add(new BillItem("Steak", new List<String>() { "Ryan" }, 30.00F));
+            orders.Add(new BillItem("Dessert", new List<String>() { "Mia", "Ryan" }, 10.00F, "No whipped cream", 0.00F));
 
-            combinedBill = new BillInfo();
+            combinedBill = new BillInfo("combined");
             foreach (BillItem item in orders)
             {
                 combinedBill.addItem(item);
             }
+            combinedBill.setPrice();
             billsPanel.Children.Add(combinedBill);
 
             profileBills = new List<BillInfo>();
-            foreach (object name in profiles)
+            foreach (String name in profiles)
             {
-                profileBills.Add(new BillInfo());
+                profileBills.Add(new BillInfo(name));
             }
 
             isSplit = false;
@@ -58,8 +60,17 @@ namespace horizontal
 
         private void requestBill_Click(object sender, RoutedEventArgs e)
         {
+            splitButton.IsEnabled = false;
             requestBill.IsEnabled = false;
-            requestBill.Content = "Bill on its way!";
+            requestBill.Content = "Bill(s) Request Sent!";
+
+            foreach (BillInfo bill in profileBills)
+            {
+                bill.requestButton.IsEnabled = false;
+                bill.requestButton.Content = "Bill Request Sent!";
+            }
+            combinedBill.requestButton.IsEnabled = false;
+            combinedBill.requestButton.Content = "Bill Request Sent!";
         }
 
         private void splitButton_Click(object sender, RoutedEventArgs e)
@@ -67,11 +78,13 @@ namespace horizontal
             if (isSplit)
             {
                 splitButton.Content = "Split the Bill";
+                requestBill.Content = "Request Bill";
                 combineBills();
             }
             else
             {
                 splitButton.Content = "Combine Bills";
+                requestBill.Content = "Request All Bills";
                 //splitBill();
                 foreach (BillInfo bill in profileBills)
                 {
@@ -89,9 +102,30 @@ namespace horizontal
                 String profile = profiles[i];
 
                 foreach (BillItem item in combinedBill.items) {
-                    if (item.users[0] == profile) {
+                    if ((item.users.Count == 1) && (item.users[0] == profile)) {
                         profileBills[i].addItem(new BillItem(item.item, new List<String>() {}, item.price, item.mods, item.modsPrice));
                     }
+                    else if (item.users[0] == profile)
+                    {
+                        splitWithMultiple(item);
+                    }
+                    profileBills[i].setPrice();
+                    profileBills[i].setName(profiles[i]);
+                }
+            }
+        }
+
+        private void splitWithMultiple(BillItem item)
+        {
+            int numPeople = item.users.Count;
+            String fractionString = "1/" + numPeople + " ";
+            String nameString = fractionString + item.item;
+
+            foreach (BillInfo bill in profileBills)
+            {
+                if (item.users.Contains(bill.user))
+                {
+                    bill.addItem(new BillItem(nameString, new List<String>() { }, (item.price / numPeople), item.mods, (item.modsPrice / numPeople)));
                 }
             }
         }
@@ -100,6 +134,26 @@ namespace horizontal
         {
             billsPanel.Children.Clear();
             billsPanel.Children.Add(combinedBill);
+        }
+
+        private void addItem(BillItem item)
+        {
+            orders.Add(item);
+            combinedBill.addItem(item);
+            combinedBill.setPrice();
+
+            foreach (BillInfo bill in profileBills)
+            {
+                if ((item.users.Count == 1) && (bill.user == item.users[0]))
+                {
+                    bill.addItem(new BillItem(item.item, new List<String>() {}, item.price, item.mods, item.modsPrice));
+                    bill.setPrice();
+                }
+                else
+                {
+                    splitWithMultiple(item);
+                }
+            }
         }
     }
 }
--- horizontal/horizontal/BillInfo.xaml.cs
+++ horizontal/horizontal/BillInfo.xaml.cs
@@ -21,12 +21,14 @@ namespace horizontal
     public partial class BillInfo : UserControl
     {
         public List<BillItem> items;
+        public String user;
 
-        public BillInfo()
+        public BillInfo(String userName)
         {
             InitializeComponent();
 
             items = new List<BillItem>();
+            user = userName;
 
         }
 
@@ -39,6 +41,32 @@ namespace horizontal
         public void removeItem(BillItem item)
         {
             billPanel.Children.Remove(item);
+        }
+
+        public void setName(String name)
+        {
+            billTitle.Content = name + "'s Bill:";
+            this.user = name;
+        }
+
+        public void setPrice()
+        {
+            float total = 0.0F;
+
+            foreach (BillItem item in items)
+            {
+                total = total + item.price;
+                total = total + item.modsPrice;
+            }
+
+            String totalString = "$" + total.ToString("0.00");
+            requestButton.Content = "Request this Bill (" + totalString + ")";
+        }
+
+        private void requestButton_Click(object sender, RoutedEventArgs e)
+        {
+            requestButton.IsEnabled = false;
+            requestButton.Content = "Bill Request Sent!";
         }
     }
 
